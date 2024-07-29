599b1bf6ca0f30f4c7f0fd4d220d793e73cee361
sodiqOladeni <sodiqoladeni@gmail.com>
2024-07-29 09:48:34 +0100

    - Update demo to release 1.0.6

--- example/settings.gradle.kts
+++ example/settings.gradle.kts
@@ -16,6 +16,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/commangopay-1318")
+        }
     }
 }
 
